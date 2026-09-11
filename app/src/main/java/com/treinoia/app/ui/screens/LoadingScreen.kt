5693f33d1d1fab493ef84bfd3e5aaf743c37dcc9
zezegabriel <108241629+zezegabriel@users.noreply.github.com>
2026-09-11 15:01:24 -0300

    Update LoadingScreen.kt

--- app/src/main/java/com/treinoia/app/ui/screens/LoadingScreen.kt
+++ app/src/main/java/com/treinoia/app/ui/screens/LoadingScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
